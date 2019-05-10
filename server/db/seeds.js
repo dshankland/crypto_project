da4698d2cd0e3fd9ccb3689118e5034fd5f05d6e
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-05-10 11:53:51 +0100

    seeds.js set up

---
+++ server/db/seeds.js
@@ -0,0 +1,35 @@
+use crypto_knight;
+db.dropDatabase();
+
+db.bookings.insertMany([
+  {
+    code: 'DASH',
+    name: 'Dash',
+    amount: 10,
+    url: ''
+  },
+  {
+    code: 'BTC',
+    name: 'Bitcoin',
+    amount: 5,
+    url: ''
+  },
+  {
+    code: 'DGB',
+    name: 'DigiByte',
+    amount: 8,
+    url: ''
+  },
+  {
+    code: 'ETH',
+    name: 'Ethereum',
+    amount: 2,
+    url: ''
+  },
+  {
+    code: 'ZEC',
+    name: 'Zcash',
+    amount: 3,
+    url: ''
+  },
+]);
