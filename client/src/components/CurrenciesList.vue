0913e7749b4f72cdb457014ae045b6c7ec015748
Darren Shankland <ds@4pq.co.uk>
2019-05-11 15:52:26 +0100

    fix index error on currencieslist

--- client/src/components/CurrenciesList.vue
+++ client/src/components/CurrenciesList.vue
@@ -1,7 +1,7 @@
 <template lang="html">
   <div v-if="shrimpy[0]" class="currencies-list">
     <h2>Currencies List</h2>
-    <CurrencyListItem v-for="currency in shrimpy" :currency="currency"/>
+    <CurrencyListItem v-for="(currency, index) in shrimpy" :currency="currency" :key="index"/>
   </div>
 </template>
 
