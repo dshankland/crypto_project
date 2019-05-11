ef62aed2fbbd807f315bfcef948a277f2409ada7
Hugh Jarvis <hugh.jarvis@blueyonder.co.uk>
2019-05-11 17:23:55 +0100

    adds wishlist figures and images

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,6 +1,7 @@
 <template lang="html">
-<div>
-  <h1>Crypto Knight</h1>
+<div class="header">
+  <img src="@/../public/images/knight.png" alt="">
+  <span><h1>Crypto Knight</h1></span>
   <h3>Leading your quest for riches</h3>
 </div>
 </template>
@@ -11,4 +12,33 @@ export default {
 </script>
 
 <style lang="css" scoped>
+/* .header {
+
+  position: absolute;
+}
+
+span {
+  display: inline;
+  position: relative;
+  left: 120px;
+}
+
+.header h1 {
+  font-family: 'UnifrakturCook', cursive;
+  font-size: 80px;
+  margin: 0;
+
+
+}
+
+.header h3 {
+  font-size: 30px;
+
+} */
+
+.header img {
+  height: 160px;
+  
+}
+
 </style>
--- client/src/components/Wishlist.vue
+++ client/src/components/Wishlist.vue
@@ -1,8 +1,23 @@
 <template lang="html">
-  <div class="wishlist">
-    <h2> At todays prices you could afford a ferrari!</h2>
+  <div class="wishlist-wrapper">
+    <h2> At todays prices you could cash it all in and buy:</h2>
     <div class="wishlist">
+      <div class="wish">
+        <img src="@/../public/images/superyacht.png" alt="">
+        <div class="figure">{{(totalValue/20000000).toFixed(0)}}</div>
+          <div class="words">superyachts</div>
+      </div>
 
+      <div class="wish"><img src="@/../public/images/ferrari.jpeg" alt="">
+      {{(totalValue/130000).toFixed(0)}} Ferrari 488s</div>
+      <div class="wish"><img src="@/../public/images/rolex.jpeg" alt="">
+      {{(totalValue/5300).toFixed(0)}} Rolex Oyster Perpetuals</div>
+      <div class="wish"><img src="@/../public/images/fendibag.jpeg" alt="">
+      {{(totalValue/3000).toFixed(0)}} Fendi bags</div>
+      <div class="wish"><img src="@/../public/images/cristal.jpeg" alt="">
+      {{(totalValue/200).toFixed(0)}} bottles of Cristal</div>
+      <div class="wish"><img src="@/../public/images/yumyum.jpeg" alt="">
+      {{(totalValue/0.8).toFixed(0)}} yum-yums</div>
     </div>
   </div>
 </template>
@@ -14,4 +29,34 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.wishlist-wrapper {
+  max-width: 650px;
+  max-height: 170px;
+
+}
+
+
+
+
+.wishlist img {
+  height: 50px;
+  display: flex;
+}
+
+.wishlist div {
+  display: inline-flex;
+}
+
+.wishlist {
+  display: flex;
+  max-height: 120px;
+  max-width:650px;
+  overflow-x: auto;
+  overflow-y: hidden;
+  flex-wrap: nowrap;
+}
+
+.wish {
+  display: inline-block;
+}
 </style>
