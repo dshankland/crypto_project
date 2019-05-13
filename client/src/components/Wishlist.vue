0d71358b697f066767bdbce8c37f2a492e51de34
Darren Shankland <ds@4pq.co.uk>
2019-05-13 20:35:22 +0100

    add toNiceNumber filter to add commas in Wishlist.vue

--- client/src/components/Wishlist.vue
+++ client/src/components/Wishlist.vue
@@ -4,24 +4,29 @@
     <div class="wishlist">
 
       <img src="@/../public/images/superyacht.png" alt="">
-      <h3><strong>{{(totalValue/20000000).toFixed(0)}}</strong> superyachts</h3>
+      <h3><strong>{{(totalValue/20000000) | toNiceNumber }}</strong> superyachts</h3>
       <img src="@/../public/images/ferrari.jpeg" alt="">
-      <h3><strong>{{(totalValue/130000).toFixed(0)}}</strong> Ferraris</h3>
+      <h3><strong>{{(totalValue/130000) | toNiceNumber }}</strong> Ferraris</h3>
       <img src="@/../public/images/rolex.jpeg" alt="">
-      <h3><strong>{{(totalValue/5300).toFixed(0)}}</strong> Rolexes</h3>
+      <h3><strong>{{(totalValue/5300) | toNiceNumber }}</strong> Rolexes</h3>
       <img src="@/../public/images/fendibag.jpeg" alt="">
-      <h3><strong>{{(totalValue/3000).toFixed(0)}}</strong> Fendi bags</h3>
+      <h3><strong>{{(totalValue/3000) | toNiceNumber }}</strong> Fendi bags</h3>
       <img src="@/../public/images/cristal.jpeg" alt="">
-      <h3><strong>{{(totalValue/200).toFixed(0)}}</strong> bottles of Cristal</h3>
+      <h3><strong>{{(totalValue/200) | toNiceNumber }}</strong> bottles of Cristal</h3>
       <img src="@/../public/images/yumyum.jpeg" alt="">
-      <h3><strong>{{(totalValue/0.8).toFixed(0)}}</strong> yum-yums</h3>
+      <h3><strong>{{(totalValue/0.8) | toNiceNumber }}</strong> yum-yums</h3>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  props: ['totalValue']
+  props: ['totalValue'],
+  filters: {
+    toNiceNumber: function (value) {
+      return new Intl.NumberFormat('en-GB').format((value.toFixed(0)));
+    }
+  }
 }
 </script>
 
